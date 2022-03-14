2a744c0362201a4a765ec36622ce47c739ca877c
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-03-14 15:34:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ offset = 0;
 let buf = Buffer.alloc(1)
 buf.writeInt8(0, 0);
 nbuf = Buffer.concat([nbuf, buf])
-offset += 1;
 //read byte
+offset += 1
 console.log(nbuf.slice(offset - 1, offset).readInt8())
 //read bool
-console.log(nbuf.slice(offset - 1, offset).readInt8() !== 0)
+console.log(nbuf.slice(offset - 1, offset).readInt8() !== 0) 
+let njbuf = Buffer.alloc(0)
+offset = 0;
+//writes a byte
+let buff = Buffer.alloc(1)
+buff.writeInt8(1, 0);
+njbuf = Buffer.concat([njbuf, buff])
+//read byte
+offset += 1
+console.log(njbuf.slice(offset - 1, offset).readInt8())
+//read bool
+console.log(njbuf.slice(offset - 1, offset).readInt8() !== 0) 
