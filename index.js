b7a8e088fc03327a3a5511e0da1b63d1f6a86019
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-03-14 15:26:27 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let nbuf = Buffer.alloc(0)
+offset = 0;
+//writes a byte
+let buf = Buffer.alloc(1)
+buf.writeInt8(0, 0);
+nbuf = Buffer.concat([nbuf, buf])
+//reads it
+offset += 1;
+//read byte
+console.log(nbuf.slice(offset - 1, offset).readInt8())
+//read bool
+console.log(nbuf.slice(offset - 1, offset).readInt8() !== 0)
