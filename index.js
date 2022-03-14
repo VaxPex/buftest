13b9d53b5ae460604f35ace41a8c660c0eb59333
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-03-14 15:35:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ offset = 0;
 let buf = Buffer.alloc(1)
 buf.writeInt8(0, 0);
 nbuf = Buffer.concat([nbuf, buf])
-//read byte
 offset += 1
+//read byte
 console.log(nbuf.slice(offset - 1, offset).readInt8())
 //read bool
 console.log(nbuf.slice(offset - 1, offset).readInt8() !== 0) 
